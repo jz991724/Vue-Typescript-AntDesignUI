c9970e01e77c1a753d7582d899faff9803377b2e
zhangjun <1548293573@qq.com>
2020-01-03 10:03:05 +0800

    20200103提交

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-
+import antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.min.css";
+import $ from "jquery";
 Vue.config.productionTip = false;
-
+Vue.use(antd);
+// Vue.use($);
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png"/>
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--      <div v-test>测试</div>-->
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+    // @ is an alias to /src
+    import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+    export default {
+        name: "home",
+        components: {
+            HelloWorld
+        },
+        directives: {
+            test: {
+                bind: function (el) {
+                  debugger
+                    alert($(el).text());
+                }
+            }
+        }
+    };
 </script>
